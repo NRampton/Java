c947f8896d80a421656250a1f91e844c4b5b92af
Nicholas Rampton <nick.rampton@gmail.com>
2018-01-05 11:04:58 -0500

    Light reorganization, FirstProgram done.

---
+++ javaFun/FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+  public static void main(String[] args) {
+    System.out.println("My name is Nick.");
+    System.out.println("I am 45 years old.");
+    System.out.println("My hometown is Salt Lake City, UT");
+  }
+}
