3e80c308b38ec7d41cf5db9cc2137e4e3131ba85
Nicholas Rampton <nick.rampton@gmail.com>
2018-01-09 16:28:09 -0500

    Working on JPS rendering and some (very) light styling. Day 45.

---
+++ springProjects/controllerspractice/src/main/java/com/codingdojo/nrampton/controllerspractice/ControllerspracticeApplication.java
@@ -0,0 +1,12 @@
+package com.codingdojo.nrampton.controllerspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerspracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerspracticeApplication.class, args);
+	}
+}
---
+++ tomcatWorkspace/JPSDemo/src/main/java/com/codingdojo/nrampton/jspdemo/JpsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.codingdojo.nrampton.jspdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpsDemoApplication.class, args);
+	}
+}
